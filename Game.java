63c4a12ec2149972a56d434f0016fa2cbad5ac65
Grantrd <31990206+Grantrd@users.noreply.github.com>
2019-12-02 20:28:14 -0500

    has game

--- Game.java
+++ Game.java
@@ -1,19 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-public class Game implements KeyListener {
+public class Game {
 
     private static final int WIDTH = 600;
     private static final int HEIGHT = 293;
     JFrame frame = new JFrame("Platformering");
     Screen screen = new Screen();
     Graphics g;
-    boolean up = false;
-    boolean down = false;
-    boolean left = false;
-    boolean right = false;
 
     public Game() {
         frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -22,32 +16,6 @@ public class Game implements KeyListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(screen);
         frame.setVisible(true);
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            screen.y -= 30;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            screen.y += 2;
-
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            screen.x -= 2;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            screen.x += 2;
-        }
-        screen.repaint();
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
     }
 
     public void gravity() {
@@ -62,20 +30,8 @@ public class Game implements KeyListener {
     }
 
     public void gameLoop() {
-        // gravity();
-        // System.out.println("hi");
-        // try {
-        // Thread.sleep(200);
-        // } catch (InterruptedException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-        // frame.addKeyListener(this);
-        // }
-        Thred t1 = new Thred(2);
-        Thred t2 = new Thred(3);
-        t1.start();
-        t2.start();
-
+        Thred t1 = new Thred(frame, screen);
+        t1.start();        
+        System.out.println("hi");
     }
 }
--- Thred.java
+++ Thred.java
@@ -1,18 +1,77 @@
-public class Thred extends Thread{
-    int a = 0;
-    int count = 0;
-    public Thred(int i)
-	{
-       
-	}
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class Thred extends Thread implements KeyListener {
+    boolean up = false;
+    boolean down = false;
+    boolean left = false;
+    boolean right = false;
+    JFrame p;
+    Screen n; 
+
+    public Thred(JFrame t, Screen q) {
+        this.p = t;
+        this.n = q;
+    }
 
     @Override
     public void run() {
-        while(true){
-            System.out.println("hi world " + a + ": " + count);
-            count++;
+        p.addKeyListener(this);
+        while (true) {
+            System.out.println(n.y);
+            System.out.println(n.x);
+            if(up)
+            {
+                n.y -= 2;
+                up = false;
+            }
+            if(down)
+            {
+                n.y += 2;
+                down = false;
+            }
+            if(left)
+            {
+                n.x -= 2;
+                left = false;
+            }
+            if(right)
+            {
+                n.x += 2;
+                right = false;
+            }
+            n.repaint();
         }
-        
-    }    
-    
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            up = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            down = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            left = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            right = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
